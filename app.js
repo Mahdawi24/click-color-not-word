52739ab11fbea6064f204c8455b2d480542cb08c
Mahdawi24 <Mahdawi24s@gmail.com>
2026-08-10 20:15:19 +0300

    defining const variables and cached elements

---
+++ app.js
@@ -0,0 +1,23 @@
+
+/*-------------------------------- Constants --------------------------------*/
+let score = 0;
+let lives = 3;
+let timeLeft = 30;
+let correctColor = '';
+/*---------------------------- Variables (state) ----------------------------*/
+
+/*------------------------ Cached Element References ------------------------*/
+const wordElement = document.querySelector('#word');
+const colorButtons = document.querySelectorAll('.color-btn');
+const messageElement = document.querySelector('#message');
+const startButton = document.querySelector('#start-btn');
+
+/*-------------------------------- Functions --------------------------------*/
+function clickStart(){
+
+}
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
+
