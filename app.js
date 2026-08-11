a65d382e7c31a8bce2bad3fbc483242d3a69dd2b
Mahdawi24 <Mahdawi24s@gmail.com>
2026-08-11 18:35:31 +0300

    adding randomColorWord that generates random color word

--- app.js
+++ app.js
@@ -13,11 +13,27 @@ const messageElement = document.querySelector('#message');
 const startButton = document.querySelector('#start-btn');
 
 /*-------------------------------- Functions --------------------------------*/
-function clickStart(){
-
+function startGame() {
+    score = 0;
+    lives = 3;
+    timeLeft = 30;
+    gameActive = true;
+    
+    
+}
+function randomColorWord(){
+const colors = ['red', 'blue', 'green', 'yellow'];
+const randomIndex = Math.floor(Math.random() * colors.length);
+//generate the random number floor round the number to greatest decimal intger number
+const randomColor = colors[randomIndex];
+//value in the colors array
+console.log(randomColor)
+}
+function gameStages(){
+    
 }
 /*----------------------------- Event Listeners -----------------------------*/
 
-
+startButton.addEventListener('click', startGame);
 
 
