cbf27f5d549c2a12809aeb2cd47ae7158a9e93ff
Mahdawi24 <Mahdawi24s@gmail.com>
2026-08-11 19:51:03 +0300

    Reforge: randomColorWord adding to it if game is active or not

--- app.js
+++ app.js
@@ -4,13 +4,18 @@ let score = 0;
 let lives = 3;
 let timeLeft = 30;
 let correctColor = '';
+let gameActive = false;
+
 /*---------------------------- Variables (state) ----------------------------*/
 
 /*------------------------ Cached Element References ------------------------*/
 const wordElement = document.querySelector('#word');
+wordElement.textContent = ' ';
 const colorButtons = document.querySelectorAll('.color-btn');
 const messageElement = document.querySelector('#message');
 const startButton = document.querySelector('#start-btn');
+const resetButtonElement = document.querySelector('#reset-btn')
+resetButtonElement.style.display = 'none';
 
 /*-------------------------------- Functions --------------------------------*/
 function startGame() {
@@ -18,22 +23,39 @@ function startGame() {
     lives = 3;
     timeLeft = 30;
     gameActive = true;
+    messageElement.textContent = '';
+    startButton.style.display = 'none';
+    resetButtonElement.style.display = 'inline';
+    randomColorWord();
     
-    
-}
-function randomColorWord(){
-const colors = ['red', 'blue', 'green', 'yellow'];
-const randomIndex = Math.floor(Math.random() * colors.length);
-//generate the random number floor round the number to greatest decimal intger number
-const randomColor = colors[randomIndex];
-//value in the colors array
-console.log(randomColor)
 }
 function gameStages(){
-    
+    while(gameActive === true){
+        
+    }
+}
+function randomColorWord(){
+ if (gameActive === true) {
+        const colors = ['red', 'blue', 'green', 'yellow'];
+        const word = colors[Math.floor(Math.random() * colors.length)];
+        const color = colors[Math.floor(Math.random() * colors.length)];
+
+        wordElement.textContent = word.toUpperCase();
+        wordElement.style.color = color;
+
+        console.log('word:', word, '| shown in:', color);
+    }
+    else{
+        wordElement.textContent = 'Start the Game!';
+        console.log(wordElement);
+        
+    }
 }
 /*----------------------------- Event Listeners -----------------------------*/
 
 startButton.addEventListener('click', startGame);
+for(let oneColorButton of colorButtons){
+    oneColorButton.addEventListener('click',randomColorWord)
+}
 
 
